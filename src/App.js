1b4d1356e83b1f5d2997b41fa768e871fe6c829b
Alline <alline_franciely@hotmail.com>
2022-05-03 16:07:54 -0300

    iniciando redux com tooljit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { value } = useSelector((state) => state.counter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {value}
+      <button>
+        Increment
+      </button>
+      <button>
+        Decrement
+      </button>
     </div>
   );
 }
