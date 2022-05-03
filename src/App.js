db54c9abc2b55ec0cd5d0d1a833537f591f98ea7
Alline <alline_franciely@hotmail.com>
2022-05-03 17:32:45 -0300

    aplicaçao pronta

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './reducers/counterSlice';
+import { useState } from 'react';
+import cat from './cat.gif';
 
 function App() {
   const { value } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const[amount, setAmount] = useState('');
   return (
     <div className="App">
-      {value}
-      <button>
-        Increment
-      </button>
-      <button>
-        Decrement
-      </button>
+      <header classname="header">
+        <h1 className="title">Tollkit Exercises Counter</h1>
+      </header>
+      <p>Quantos exercícios você deseja que o gatinho faça?</p>
+      <p>{value}</p>
+      <div className="inputs">
+        <div className="buttons">
+        <button
+          type="button"
+          className="increment"
+          onClick={ () => dispatch(increment()) }
+        >
+          Increment
+        </button>
+        <button
+          type="button"
+          className="decrement"
+          onClick={ () => dispatch(decrement()) }
+        >
+          Decrement
+        </button>
+      </div>
+      <div className="amount">
+        <label
+        htmlFor='amount-input'>
+          Adicione exercícios
+          <input
+            id='amount-input'
+            type="number"
+            onChange={  ({target}) => setAmount(target.value) }
+            value={ amount }
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ () => dispatch(incrementByAmount(+amount))}
+        >
+          Add amount
+        </button>
+      </div>
+      </div>
+      <div className="cat">
+        <img
+          src={cat}
+          alt="cat"
+        />
+      </div>
     </div>
   );
 }
